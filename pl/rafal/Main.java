d1fe57ad095ddba69e801cdcadfcbfb835117be2
RafalDaniliszyn <RafalDaniliszyn@users.noreply.github.com>
2022-05-06 16:47:08 +0200

    changed mutation

--- pl/rafal/Main.java
+++ pl/rafal/Main.java
@@ -3,7 +3,7 @@ package pl.rafal;
 public class Main {
 
     public static void main(String[] args) {
-        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm(100,30, 6, 10);
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm(100,30, 8, 10);
 
         Thread algorytm = new Thread(geneticAlgorithm);
         algorytm.start();
